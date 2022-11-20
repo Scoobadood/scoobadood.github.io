ffab2dab79be356d713c54da8de9f6953610ba9f
Dave <scoobadood@me.com>
2022-11-20 19:51:53 +1100

    FEAT: Add content to script

---
+++ javascript/nbi.js
@@ -0,0 +1,50 @@
+var posts = document.getElementById('posts')
+var grid = document.createElement('grid')
+
+var dogs=['pooch']
+
+for (dog in dogs) {
+	var article = document.createElement('article')
+	article.class='dog-card'
+
+	var body = document.createElement('div')
+	body.class='dog-card-body'
+
+	var overlay = document.createElement('div')
+	overlay.class='dog-card-overlay'
+
+	var owner_overlay = document.createElement('div')
+	owner_overlay.class=['dog-card-overlay', 'fade-in']
+
+
+
+
+            // <div class="">
+            //     <img class="dog-card-dog-image" src="../images/dog1.png" />
+            // </div>
+
+            // <div class="dog-card-overlay">
+            //     <span class="dog-card-overlay-name">Pupstar</span>
+            //     <span class="dog-card-overlay-likes">10</span>
+            // </div>
+
+            // <div class="dog-card-owner-overlay fade-in">
+            //     <img class="dog-card-owner-avatar" src="../images/avatar.png"/>
+            //     <div class="dog-card-owner-name">Dave</div>
+            // </div>
+
+    article.append(body)
+    article.append(overlay)
+    article.append(owner_overlay)
+
+	grid.append(article)
+}
+
+
+
+
+
+posts.append(grid)
+
+
+
