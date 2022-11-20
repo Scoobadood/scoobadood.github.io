91cafb2527d7e97f8640395124247a901db75d68
Dave <scoobadood@me.com>
2022-11-20 19:58:05 +1100

    FEAT: Renamed style directory, made JS a function and added to HTML

--- javascript/nbi.js
+++ javascript/nbi.js
@@ -1,58 +1,58 @@
-var posts = document.getElementById('posts')
-var grid = document.createElement('grid')
+function add_dogs() {
+	var posts = document.getElementById('posts')
+	var grid = document.createElement('grid')
 
-var dogs=['pooch']
+	var dogs=['pooch']
 
-for (dog in dogs) {
-	var article = document.createElement('article')
-	article.class='dog-card'
-
-
-
-	var body = document.createElement('div')
-	body.class='dog-card-body'
-	var img = document.createElement('img')
-	img.class='dog-card-dog-image'
-	img.src='images/dog1.png'
-	body.append(img)
+	for (dog in dogs) {
+		var article = document.createElement('article')
+		article.class='dog-card'
 
 
 
-	var overlay = document.createElement('div')
-	overlay.class='dog-card-overlay'
+		var body = document.createElement('div')
+		body.class='dog-card-body'
+		var img = document.createElement('img')
+		img.class='dog-card-dog-image'
+		img.src='images/dog1.png'
+		body.append(img)
 
-	var owner_overlay = document.createElement('div')
-	owner_overlay.class=['dog-card-overlay', 'fade-in']
+
+
+		var overlay = document.createElement('div')
+		overlay.class='dog-card-overlay'
+
+		var owner_overlay = document.createElement('div')
+		owner_overlay.class=['dog-card-overlay', 'fade-in']
 
 
 
 
-            // <div class="">
-            //     <img class="dog-card-dog-image" src="../images/dog1.png" />
-            // </div>
+	            // <div class="">
+	            //     <img class="dog-card-dog-image" src="../images/dog1.png" />
+	            // </div>
 
-            // <div class="dog-card-overlay">
-            //     <span class="dog-card-overlay-name">Pupstar</span>
-            //     <span class="dog-card-overlay-likes">10</span>
-            // </div>
+	            // <div class="dog-card-overlay">
+	            //     <span class="dog-card-overlay-name">Pupstar</span>
+	            //     <span class="dog-card-overlay-likes">10</span>
+	            // </div>
 
-            // <div class="dog-card-owner-overlay fade-in">
-            //     <img class="dog-card-owner-avatar" src="../images/avatar.png"/>
-            //     <div class="dog-card-owner-name">Dave</div>
-            // </div>
+	            // <div class="dog-card-owner-overlay fade-in">
+	            //     <img class="dog-card-owner-avatar" src="../images/avatar.png"/>
+	            //     <div class="dog-card-owner-name">Dave</div>
+	            // </div>
 
-    article.append(body)
-    article.append(overlay)
-    article.append(owner_overlay)
+	    article.append(body)
+	    article.append(overlay)
+	    article.append(owner_overlay)
 
-	grid.append(article)
+		grid.append(article)
+	}
+
+
+
+
+
+	posts.append(grid)
 }
-
-
-
-
-
-posts.append(grid)
-
-
 
